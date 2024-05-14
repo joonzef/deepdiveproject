17a0058c16c4f305fe10d418140ebd19b42e0767
Youssef <joonzef@gmail.com>
2024-05-15 00:22:34 +0200

    hei

---
+++ joons library/JoonsLibrary/src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+import Navbar from './components/NavBar.vue'
+</script>
+
+<template>
+  <Navbar />
+</template>
+
---
+++ joons library/JoonsLibrary/src/components/NavBar.vue
@@ -0,0 +1,12 @@
+<template>
+    <nav>
+        <div class="flex flex-row">
+            <div class="logo">
+                <img src="../assets/logo.png" width="150px">
+            </div>
+            <div class="">
+                <input type="text" placeholder="Search..">
+            </div>
+        </div>
+    </nav>
+</template>
