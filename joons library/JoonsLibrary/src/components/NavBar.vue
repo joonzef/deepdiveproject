20c948465a9026511c61f60ccf139b6fee010de1
Youssef <joonzef@gmail.com>
2024-05-23 15:26:02 +0200

    -a

--- joons library/JoonsLibrary/src/components/NavBar.vue
+++ joons library/JoonsLibrary/src/components/NavBar.vue
@@ -1,12 +1,31 @@
 <template>
-    <nav>
-        <div class="flex flex-row">
-            <div class="logo">
-                <img src="../assets/logo.png" width="150px">
-            </div>
-            <div class="">
-                <input type="text" placeholder="Search..">
-            </div>
-        </div>
-    </nav>
+
+
+<nav class="bg-white border-gray-200 dark:bg-gray-900">
+  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+    <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
+        <img src="../assets/logo.png" class="h-14" alt="joonsLibrary Logo" />
+        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Joons Library</span>
+    </a>
+    <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+        <span class="sr-only">Open main menu</span>
+        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+        </svg>
+    </button>
+    <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+      <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+        <li>
+          <a href="#" class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
+        </li>
+        <li>
+            <input type="text" id="email-adress-icon" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2" placeholder="Search...">
+        </li>
+        <li class="mr-3">
+            <a class="inline-block border border-blue-500 rounded py-1 px-3 bg-blue-500 text-white" href="#">Login</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
 </template>
