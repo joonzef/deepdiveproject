e1753246214e49684dc110cf14aeee921cd3455a
Youssef <joonzef@gmail.com>
2024-06-04 15:25:41 +0200

    hei

--- joons library/JoonsLibrary/src/App.vue
+++ joons library/JoonsLibrary/src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
 import Navbar from './components/NavBar.vue'
+import Api from './components/Api.vue'
 </script>
 
 <template>
   <Navbar />
+  <api />
 </template>
 
---
+++ joons library/JoonsLibrary/src/components/Api.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  methods: {
+    async fetchTwitch() {
+      try {
+        const response = await fetch(`https://id.twitch.tv/oauth2/token?client_id=3m8bnxaiypl0zoe0g1pazr8an1l0po&client_secret=n0rmv96v6ux6v1zlasqizl6nrv5g6r&grant_type=client_credentials`);
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        console.log(data);
+      } catch (error) {
+        console.error('An error occurred while fetching the Twitch token:', error);
+      }
+    }
+  }
+}
+</script>
