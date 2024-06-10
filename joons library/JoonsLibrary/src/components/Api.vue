0ce383b98fe90b3c2cc9300d06168242925815d4
Youssef <joonzef@gmail.com>
2024-06-10 11:45:45 +0200

    hei

--- joons library/JoonsLibrary/src/components/Api.vue
+++ joons library/JoonsLibrary/src/components/Api.vue
@@ -1,9 +1,17 @@
+<template>
+  <div>
+  </div>
+</template>
+
 <script>
 export default {
   methods: {
     async fetchTwitch() {
       try {
-        const response = await fetch(`https://id.twitch.tv/oauth2/token?client_id=3m8bnxaiypl0zoe0g1pazr8an1l0po&client_secret=n0rmv96v6ux6v1zlasqizl6nrv5g6r&grant_type=client_credentials`);
+        console.log(import.meta.env.VITE_FORTNITE);
+        const response = await fetch(import.meta.env.VITE_TWITCH_LINK, {
+          method: 'POST'
+        });
 
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
@@ -15,6 +23,9 @@ export default {
         console.error('An error occurred while fetching the Twitch token:', error);
       }
     }
+  },
+  created() {
+    this.fetchTwitch();
   }
 }
 </script>
